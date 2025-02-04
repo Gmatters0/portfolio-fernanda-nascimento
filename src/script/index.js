7df706045f7d689fd5dafa64e2e81e29362d3daf
Gmatters0 <gabrielmaranhao.cpm@gmail.com>
2025-02-03 22:44:06 -0300

    feat: Archives Section (in progress)

--- src/script/index.js
+++ src/script/index.js
@@ -20,3 +20,22 @@ window.addEventListener('scroll', () => {
         nav.style.boxShadow = 'none';
     }
 });
+
+function changeArchiveState() {
+    const arrow = document.querySelector('.arrow')
+    const archive = document.querySelector('.archive-icon')
+    const itensList = document.querySelector('.archives-itens-list')
+
+    if (arrow.classList.contains("rotate")) {
+        arrow.classList.remove("rotate")
+        archive.src = "./icons/openArchive.svg"
+        itensList.classList.add('expanded');
+        itensList.classList.remove('collapsed');
+    } else {
+        arrow.classList.add("rotate")
+        archive.src = "./icons/closedArchive.svg"
+        itensList.classList.add('collapsed');
+        itensList.classList.remove('expanded');
+    }
+
+}
